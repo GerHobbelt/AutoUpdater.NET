a254b4080d0defe97926ca6333ef9042a7364074
Sarmiento, Alberto <asarmiento13315@gmaill.com>
2018-06-26 16:47:29 -0400

    fixing appveyor build script, extracted build bath

---
+++ build.cmd
@@ -0,0 +1,18 @@
+set BuildFolder=%1
+set Logger=%2
+set ZipExtractorProj="%BuildFolder%\ZipExtractor\ZipExtractor.csproj"
+set AutoUpdaterProj="%BuildFolder%\AutoUpdater.NET\AutoUpdater.NET.csproj"
+set UnitTestsProj="%BuildFolder%\UnitTests\UnitTests.csproj"
+
+set list=
+set list=%list%;Release
+set list=%list%;Release-NET35
+set list=%list%;Release-NET40
+set list=%list%;Release-NET452
+set list=%list%;Release-NET462
+
+for %%config in (%list%) do (
+  msbuild %ZipExtractorProj% /p:Configuration=%%config /verbosity:minimal /logger:%Logger% || EXIT 1
+  msbuild %AutoUpdaterProj% /p:Configuration=%%config /verbosity:minimal /logger:%Logger% || EXIT 1
+  msbuild %UnitTestsProj% /p:Configuration=%%config /verbosity:minimal /logger:%Logger% || EXIT 1
+)
